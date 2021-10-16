d5b7d934a782c89e4640d86271b3fe740936baf3
naoto-honda <82433229+naoto-honda@users.noreply.github.com>
2021-10-16 18:39:02 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
 $(function() {
+
+  // クリックしたら該当セクションへスライド
   $('a[href^=#]').click(function() {
-
     var speed = 500;
-
     var href = $(this).attr("href");
-
     var target = $(href == "#" || href == "" ? 'html' : href);
-
     var position = target.offset().top - 50;
-
     $("html, body").animate({scrollTop:position}, speed, "swing");
-
     return false;
   });
+
+  // ウィンドウをスクロールすると下からふわっとコンテンツがフェードイン
+  $(window).on('load scroll',function (){
+		$('.animation').each(function(){
+			//ターゲットの位置を取得
+			var target = $(this).offset().top;
+			//スクロール量を取得
+			var scroll = $(window).scrollTop();
+			//ウィンドウの高さを取得
+			var height = $(window).height();
+			//ターゲットまでスクロールするとフェードインする
+			if (scroll > target - height){
+				//クラスを付与
+				$(this).addClass('active');
+			}
+		});
+	});
+
+
 });
